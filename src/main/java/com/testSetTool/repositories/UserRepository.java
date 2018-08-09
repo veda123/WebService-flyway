0f72fdb147a8da3f3da9ac419bad8bbb83c6077b
Stephane <stephane@earis.com>
2018-08-09 14:25:21 -0700

    removed repository annotation

--- src/main/java/com/testSetTool/repositories/UserRepository.java
+++ src/main/java/com/testSetTool/repositories/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import com.testSetTool.entities.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByEmail(String email);
